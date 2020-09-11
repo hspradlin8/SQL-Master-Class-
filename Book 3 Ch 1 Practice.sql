04e5aac461b540927083357028ebb444c059c196
Heidi Smith <52766359+hspradlin8@users.noreply.github.com>
2020-09-11 09:51:07 -0500

    Add files via upload

---
+++ Book 3 Ch 1 Practice.sql
@@ -0,0 +1,17 @@
+-- Practice: Employees
+-- Rheta Raymen an employee of Carnival has asked to be transferred to a 
+-- different dealership location. She is currently at dealership 751. 
+-- She would like to work at dealership 20. Update her record to reflect her transfer.
+UPDATE dealershipemployees 
+SET dealership_id = 20
+WHERE employee_id = 680 AND dealership_id = 751;
+
+
+-- Practice: Sales
+-- A Sales associate needs to update a sales record because her customer 
+-- want so pay wish Mastercard instead of American Express. Update Customer, 
+-- Layla Igglesden Sales record which has an invoice number of 2781047589.
+
+UPDATE sales 
+SET payment_method = 'mastercard'
+WHERE customer_id = 13;
